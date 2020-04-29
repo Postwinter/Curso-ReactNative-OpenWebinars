b595222538f3e20f75d482f0f00f3b1396284dcb
Carlos <winter21123@gmail.com>
2020-04-29 12:24:33 +0200

    Version 1.4

--- src/data/todos/index.js
+++ src/data/todos/index.js
@@ -1,24 +1,19 @@
 import uuid from "todoList/node_modules/react-native-uuid";
+import {AsyncStorage} from "react-native";
 
+const TODOS_KEY = "@MyStore:todos";
 const delay = ms => new Promise(res => setTimeout(res,ms));
 
 const getTodos = async () => {
   await delay(2000); //Aplicar espera de 2 segundos
-  return [
-    newTodo({ text: "Elemento 1", done: false, priority: 2 }), 
-    newTodo({ text: "Elemento 2", done: false, priority: 2 }), 
-    newTodo({ text: "Elemento 3", done: false, priority: 2 }),
-    newTodo({ text: "Elemento 4", done: true, priority: 2 }),
-    newTodo({ text: "Elemento 5", done: false, priority: 2 }),
-    newTodo({ text: "Elemento 6", done: false, priority: 2 }),
-    newTodo({ text: "Elemento 7", done: true, priority: 2 }),
-    newTodo({ text: "Elemento 8", done: false, priority: 2 }),
-    newTodo({ text: "Elemento 9", done: false, priority: 2 }),
-    newTodo({ text: "Elemento 10", done: false, priority: 2 }),
-    newTodo({ text: "Elemento 11", done: true, priority: 2 }),
-    newTodo({ text: "Elemento 12", done: false, priority: 2 }),
-    newTodo({ text: "Elemento 13", done: false, priority: 2 })
-  ]
+  let todos = [];
+  try {
+    todos = await AsyncStorage.getItem(TODOS_KEY);
+  } catch (error) {
+    // Error retrieving data
+    console.log(error.message);
+  }
+  return JSON.parse(todos);
 };
 
 //Crea uno nuevo
@@ -35,13 +30,13 @@ const updateTodo = (list, todo) => {
   const updateIndex = list.findIndex(t => t.id === todo.id);
   const newTodoList = [...list];
   newTodoList[updateIndex] = todo;
-  //saveTodos(newTodoList);
+  saveTodos(newTodoList);
   return newTodoList;
 };
 
 const addTodo = (list, todo) => {
   const newTodoList = [...(list || []), newTodo(todo)];
-  //saveTodos(newTodoList);
+  saveTodos(newTodoList);
   return newTodoList;
 };
 
@@ -56,7 +51,7 @@ const saveTodos = async todos => {
 
 const deleteTodo = (list, todo) => {
   const newTodoList = list.filter(t => t.id !== todo.id);
-  //saveTodos(newTodoList);
+  saveTodos(newTodoList);
   return newTodoList;
 };
 
